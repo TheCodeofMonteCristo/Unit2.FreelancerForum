612969c1fa9e1fab5466e66fd0a12676ff9a5454
Shane A. Healy <ShaneAHealy55@gmail.com>
2024-09-17 08:37:04 -0500

    Placeholder status of HTML, CSS, JavaScript

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+//Template Sample Code On How to Create Frelancer Object
+
+
+/*
+const freelancers = [
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+  ];
+
+
+*/
+
+
+
+
+  const freelancersObject = [
+    { name: "Professor X", price: 25, occupation: "gardener" },
+    { name: "Cyclops", price: 51, occupation: "programmer" },
+    { name: "Iceman", price: 43, occupation: "teacher" },
+    { name: "Beast", price: 81, occupation: "teacher" },
+    { name: "Angel/Archangel", price: 43, occupation: "teacher" },
+    { name: "Marvel Girl/Phoenix", price: 76, occupation: "programmer" },
+    { name: "Nightcrawler", price: 47, occupation: "teacher" },
+    { name: "Colossus", price: 72, occupation: "driver" },
+  ];
